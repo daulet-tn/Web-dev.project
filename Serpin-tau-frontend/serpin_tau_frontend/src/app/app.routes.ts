29f8420da7c6e654827ad37b4bb6bb282561a5fd
daulet_tn <daulettanirberdi94@gmail.com>
2026-04-09 18:29:03 +0500

    all pages structure ready

--- Serpin-tau-frontend/serpin_tau_frontend/src/app/app.routes.ts
+++ Serpin-tau-frontend/serpin_tau_frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+import { Tours } from './pages/tours/tours';
+import { Posts } from './pages/posts/posts';
+import { Profile } from './pages/profile/profile';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'tours', component: Tours },
+  { path: 'posts', component: Posts },
+  { path: 'profile', component: Profile }
+];
